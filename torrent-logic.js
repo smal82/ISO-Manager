a788be696c7ba52e3da4c8685d1f588b1d44c0e3
smal <47319874+smal82@users.noreply.github.com>
2026-01-06 22:56:41 +0100

    Update torrent-logic.js

--- torrent-logic.js
+++ torrent-logic.js
@@ -10,8 +10,8 @@ window.uploadSpeedAllocator = {
     
     // Calcola e distribuisce la banda upload tra tutti i torrent
     allocate: function() {
-        const countActive = parseInt($('#count-active').text(), 10);
-        const countSeeding = parseInt($('#count-seeding').text(), 10);
+        const countActive = parseInt($('#count-active').text(), 10) || 0;
+        const countSeeding = parseInt($('#count-seeding').text(), 10) || 0;
         
         let totalUpNodes = 0;
         
